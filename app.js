5d07253a923a4b31c235f59e7cc783fd286bdb89
Eugene <joker3lolz@gmail.com>
2024-02-09 16:00:56 +0300

    fix: write file

--- app.js
+++ app.js
@@ -8,17 +8,13 @@ const outputFile = process.env.OUTPUT_FILE;
 const apiKey = process.env.GEOCODER_API_KEY;
 
 async function run() {
-  console.log(`Input file: ${process.env.INPUT_FILE}`);
-console.log(`Output file: ${process.env.OUTPUT_FILE}`);
-console.log(`API Key: ${process.env.GEOCODER_API_KEY}`);
-
   try {
     const addresses = await readFile(inputFile);
     console.log(`Read file, ${addresses.length} addresses now available`);
 
     try {
       const geoData = await getLocationData(apiKey, addresses);
-      console.log(`Geocoded addresses at ${JSON.stringify(geoData)}`);
+      console.log(`Geocoded addresses at geoData`);
 
       try {
         await writeFile(geoData, outputFile);
